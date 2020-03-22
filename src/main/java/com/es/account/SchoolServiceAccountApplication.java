90876788423dc2ef11b2f2ef1cfb4ccf45f5318e
weixiaoxing <weixiaoxing@meishubao.com>
2020-03-22 23:44:15 +0800

    update

--- src/main/java/com/es/account/SchoolServiceAccountApplication.java
+++ src/main/java/com/es/account/SchoolServiceAccountApplication.java
@@ -4,7 +4,8 @@ import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,13 +14,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static org.springframework.data.repository.query.QueryLookupStrategy.Key.CREATE_IF_NOT_FOUND;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableJpaAuditing
 @EnableAsync
 @EntityScan(basePackages = {"com.es.account.entity"})
 @EnableJpaRepositories(basePackages = "com.es.account.repository", queryLookupStrategy = CREATE_IF_NOT_FOUND)
 @EnableSwagger2
 @EnableJSONDoc
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {
+        "com.es.account.service"
+})
 public class SchoolServiceAccountApplication {
 
     public static void main(String[] args) {
