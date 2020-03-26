963e3f0b3151fdc62ef671caedbe8be6e5692e2f
WeisonWei <weixx3@126.com>
2020-03-26 20:43:14 +0800

    add Dockerfile

--- deploy-image-maven.sh
+++ deploy-image-maven.sh
@@ -3,6 +3,6 @@ params="-DskipTests -P local-nexus-repo,registry2,!build-swagger2markup-asciidoc
 
 DIR=`dirname $0`
 echo ${DIR}
-#cd ${DIR}/../school-service-account/ && ./mvnw clean install ${params}
+#cd ${DIR}/../e-school-service-account/ && ./mvnw clean install ${params}
 #cd ${DIR}/ && ./mvnw  clean deploy ${params}
 cd ${DIR}/../e-school-service-account/ && ./mvnw clean deploy ${params}
